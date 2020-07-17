a32cd3ef9db6ae7e6ff7ab41552737fe17ad18b4
Ruslan Butyk <movebytes@gmail.com>
2020-07-17 17:38:28 +0300

    chore(ci): add atomic steps for tests

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,12 @@
 import { browser, by, element } from 'protractor';
+import {WebElementPromise} from 'selenium-webdriver';
 
 export class AppPage {
   navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getMapElement(): WebElementPromise {
+    return element(by.css('#map')).getWebElement();
   }
 }
